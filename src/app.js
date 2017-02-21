fe6a0c367d08064030f24b0013f9b23f237c1ce0
Cary <carymeskell@gmail.com>
2017-02-20 16:09:33 -0800

    Added routes and set up contact page, sorta

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
-var app = angular.module('app', []);
+
+var app = angular.module('app', ['ngRoute'])
+.config(function ($routeProvider, $locationProvider) {
+  $locationProvider.html5Mode(true);
+  // $locationProvider.hashPrefix('!');
+
+  $routeProvider
+  .when('/about', {
+    templateUrl: './src/directives/about/about.html'
+  })
+  .when('/contact', {
+    templateUrl: './src/directives/contact/contact.html'
+  })
+  .when('/', {
+    templateUrl: './src/directives/main/main.html'
+  })
+  .otherwise({
+    redirectTo: '/'
+  });
+});
